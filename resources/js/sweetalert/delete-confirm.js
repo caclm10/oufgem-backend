cc4fa771cc3149e0dd864c900196934b99522d60
caclm10 <lewinxander@gmail.com>
2022-08-15 11:55:03 +0700

    refactor: delete confirmation

---
+++ resources/js/sweetalert/delete-confirm.js
@@ -0,0 +1,29 @@
+const deleteForms = document.querySelectorAll(`[data-delete-form]`)
+
+const deleteSwal = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-danger ms-3',
+        cancelButton: 'btn btn-secondary',
+    },
+    buttonsStyling: false,
+    showCancelButton: true,
+    confirmButtonText: 'Simpan',
+    cancelButtonText: 'Batal',
+    icon: 'warning',
+    reverseButtons: true
+})
+
+for (const deleteForm of deleteForms) {
+    deleteForm.addEventListener('submit', event => {
+        event.preventDefault()
+
+        deleteSwal.fire({
+            title: deleteForm.dataset.message,
+        })
+            .then(result => {
+                if (result.isConfirmed) {
+                    deleteForm.submit()
+                }
+            })
+    })
+}
